ec8097e7d7c7e7ce6fc36fa78535b6694de4fcce
Ankit Prajapati <ankit.prajapati@mindinventory.com>
2026-01-28 12:40:52 +0530

    feat: enhance Banner component with animated image loading and skeleton display

--- app/src/sdui/v1/components/Banner.v1.tsx
+++ app/src/sdui/v1/components/Banner.v1.tsx
@@ -10,6 +10,11 @@ import {
   LayoutConfig,
 } from "@/src/types"
 import Skeleton from "react-native-reanimated-skeleton"
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated"
 
 const { width } = Dimensions.get("window")
 
@@ -21,26 +26,31 @@ interface BannerV1Props {
 
 export const BannerV1 = React.memo(
   ({ config, layout, tokens }: BannerV1Props) => {
-    // console.log("layout: ", layout)
+    const imageOpacity = useSharedValue(0)
+
     const [bannerData, setBannerData] = useState<BannerPayload | null>(null)
+
     const [loading, setLoading] = useState(true)
+    const [imageLoading, setImageLoading] = useState(true)
 
     const getBannerData = useCallback(async () => {
       if (!config.api) return
 
-      // setLoading(true)
+      setLoading(true)
       try {
         const bannerRes = await fetchBanner()
 
         if (!bannerRes || !bannerRes.payload) {
           throw new Error("Invalid banner response")
         }
+        // ⏳ delay UI update
+        // await new Promise((resolve) => setTimeout(resolve, 1000))
 
         setBannerData(bannerRes.payload)
       } catch (error) {
         console.error("Failed to fetch banner data:", error)
       } finally {
-        setLoading(false)
+        // setLoading(false)
       }
     }, [config.api])
 
@@ -48,43 +58,87 @@ export const BannerV1 = React.memo(
       getBannerData()
     }, [getBannerData])
 
+    const animatedStyle = useAnimatedStyle(() => {
+      return {
+        opacity: imageOpacity.value,
+      }
+    })
+
+    const showSkeleton = loading || imageLoading || !bannerData
+
     return (
       <SectionWrapper layout={layout} tokens={tokens}>
-        {loading ? (
-          <BannerSkeleton />
-        ) : (
-          <SectionItemWrapper layout={layout} tokens={tokens}>
-            <Image
-              source={{ uri: bannerData?.image }}
-              style={{ height: "100%", borderRadius: 10 }}
+        <SectionItemWrapper layout={layout} tokens={tokens}>
+          {showSkeleton && <BannerSkeleton />}
+
+          {bannerData && (
+            <Animated.Image
+              source={{ uri: bannerData.image }}
+              resizeMode="cover"
+              onLoadStart={() => {
+                imageOpacity.value = 0
+                setImageLoading(true)
+              }}
+              onLoadEnd={() => {
+                imageOpacity.value = withTiming(1, {
+                  duration: 250,
+                })
+                setImageLoading(false)
+              }}
+              onError={() => setImageLoading(false)}
+              style={[
+                {
+                  width: "100%",
+                  height: "100%",
+                  borderRadius: 10,
+                  position: "absolute",
+                },
+                animatedStyle,
+              ]}
             />
-            {config.title && <Text>{config.title}</Text>}
-          </SectionItemWrapper>
-        )}
+          )}
+
+          {config.title && <Text style={{ marginTop: 8 }}>{config.title}</Text>}
+          {/* {loading && !bannerData ? (
+            <BannerSkeleton />
+          ) : (
+            <>
+              <Image
+                source={{ uri: bannerData?.image }}
+                style={{ height: "100%", borderRadius: 10 }}
+                 onLoadStart={() => setImageLoading(true)}
+              onLoadEnd={() => setImageLoading(false)}
+              onError={() => setImageLoading(false)}
+              />
+              {config.title && <Text>{config.title}</Text>}
+            </>
+          )} */}
+        </SectionItemWrapper>
       </SectionWrapper>
     )
   },
 )
 
 const BannerSkeleton = () => {
-  console.log("calling !!!!")
+  // console.log("calling !!!!")
 
   return (
     <View
       style={{
-        paddingHorizontal: 16,
-        marginTop: 12,
+        height: "100%",
+        width: "100%",
       }}>
       <Skeleton
         isLoading={true}
         layout={[
           {
             key: "banner",
-            width: width - 32,
-            height: 180,
-            borderRadius: 16,
-            marginBottom: 16,
-            marginTop: 10,
+            // width: width - 32,
+            height: "100%",
+            width: "100%",
+            borderRadius: 10,
+            // marginBottom: 16,
+            // marginTop: 10,
           },
         ]}
       />
